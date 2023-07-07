e484185e6f0e81c4af15e4e602a86c0e17afd3c8
d1eg0 <84401100+diegocastroh10@users.noreply.github.com>
2023-07-07 02:54:53 -0400

    :hammer: add component providerbars

--- src/app/home/provider/page.tsx
+++ src/app/home/provider/page.tsx
@@ -1,10 +1,16 @@
 import LayoutSidebar from "@fooddy/components/LayoutSidebar"
+import ProviderBars from "@fooddy/components/ProviderBars";
 import ProviderStats from "@fooddy/components/ProviderStats";
 
 export default function ProviderPage() {
     return (
         <LayoutSidebar>
-            <ProviderStats />
+            <main className="min-h-screen">
+                <ProviderStats />
+                <div className="p-4 grid md:grid-cols-3 grid-cols-1 gap-4">
+                    <ProviderBars />
+                </div>
+            </main>
         </LayoutSidebar>
     );
 };
---
+++ src/components/ProviderBars.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ProviderBars = () => {
+    return (
+        <div>Bar</div>
+    )
+}
+
+export default ProviderBars;
